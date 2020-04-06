bf639cee6578979a2165c06cf1c4d9dfe872c9a5
Diego Stiehl <seccomiro@gmail.com>
2020-04-06 14:45:43 -0300

    Make search action
    Also using axios to make the GET request.

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,4 +1,8 @@
 class SearchController < ApplicationController
   def index
   end
+
+  def search
+    render json: { sucess: true }
+  end
 end
--- app/javascript/packs/search.js
+++ app/javascript/packs/search.js
@@ -1,20 +1,24 @@
 const _ = require('underscore');
+const axios = require('axios');
 
-const debounceSearch = _.debounce((term) => {
-  console.log(`Search: ${term}`);
+const debounceSearch = _.debounce(async (term, baseUrl) => {
+  const url = `${baseUrl}/${term}`;
+  const response = await axios.get(url);
+  console.log(response.data);
 }, 500);
-
-const search = (term) => {
-  debounceSearch(term);
-};
 
 window.onload = (e) => {
   const form = document.querySelector('.search_form');
   const field = document.querySelector('#term');
+  const baseUrl = form.getAttribute('action');
+  console.log(baseUrl);
+
+  const search = (term) => {
+    debounceSearch(term, baseUrl);
+  };
 
   form.addEventListener('submit', (e) => {
     e.preventDefault();
-
     search(field.value);
   });
 
