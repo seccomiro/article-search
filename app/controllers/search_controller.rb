f902fe555d1b4d33499b73243b007267a4c70941
Diego Stiehl <seccomiro@gmail.com>
2020-04-06 15:03:02 -0300

    Make Redis storing first attempt

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -3,6 +3,20 @@ class SearchController < ApplicationController
   end
 
   def search
-    render json: { sucess: true }
+    redis = Redis.new
+    count = redis.incr("count")
+    redis.set(
+      count,
+      {
+        term: params[:term],
+        ip: request.ip,
+        article_count: 0,
+        sought_at: DateTime.now.strftime("%Q"),
+      }.to_json
+    )
+    render json: { sucess: true, count: count }
+  end
+
+  def statistics
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   get 'search', to: 'search#index'
+  get 'search/statistics', to: 'search#statistics'
   get 'search/:term', to: 'search#search'
   get 'home/index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
