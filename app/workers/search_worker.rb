531e2924d0cc3ef78041f1f1470a036718e79870
Diego Stiehl <seccomiro@gmail.com>
2020-04-07 14:25:09 -0300

    Improve statistics generation

--- app/workers/search_worker.rb
+++ app/workers/search_worker.rb
@@ -1,8 +1,8 @@
 class SearchWorker
-  # require 'sidekiq-scheduler'
   include Sidekiq::Worker
 
   def perform
+    puts 'Starting search indexing...'
     redis = Redis.new
     keys = redis.scan_each(match: "ip:*").to_a.sort
     return if keys.none?
@@ -10,26 +10,26 @@ class SearchWorker
 
     searches.each do |ip, searches|
       if searches.one?
-        save_search searches.first["term"]
+        save_search searches.first["term"], searches.first["article_count"]
         next
       end
       searches[0..-2].each_with_index do |s, i|
         next_s = searches[i + 1]
         submit = s["submit"]
         if submit
-          save_search s["term"]
+          save_search s["term"], s["article_count"]
           next
         end
         diff = next_s["sought_at"].to_i - s["sought_at"].to_i
         if diff > 3000
-          save_search s["term"]
+          save_search s["term"], s["article_count"]
           next
         end
       end
       next_s = searches.last
       diff = next_s["sought_at"].to_i - searches[-2]["sought_at"].to_i
       submit = next_s["submit"]
-      save_search next_s["term"] if submit || diff > 3000
+      save_search next_s["term"], next_s["article_count"] if submit || diff > 3000
     end
 
     redis.del keys
@@ -48,10 +48,12 @@ class SearchWorker
     searches
   end
 
-  def save_search(term)
+  def save_search(term, article_count)
     search = Search.find_by_term(term)
     if search
       search.count += 1
+      search.article_count += article_count
+      search.zero_article_count += 1 if article_count == 0
       search.save
     else
       search = Search.create(term: term, count: 1)
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_07_143055) do
+ActiveRecord::Schema.define(version: 2020_04_07_171228) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
@@ -23,6 +23,8 @@ ActiveRecord::Schema.define(version: 2020_04_07_143055) do
     t.integer "count"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "article_count", default: 0
+    t.integer "zero_article_count", default: 0
   end
 
 end
