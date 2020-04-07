3cd34ac6af3841f653a40e1e32a6852902cfd79c
Diego Stiehl <seccomiro@gmail.com>
2020-04-07 11:42:08 -0300

    Add Article scaffold

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :articles
   root to: 'search#index'
   get 'search', to: 'search#search'
   get 'search/statistics', to: 'search#statistics'
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_06_191114) do
+ActiveRecord::Schema.define(version: 2020_04_07_143055) do
+
+  create_table "articles", force: :cascade do |t|
+    t.string "title"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "searches", force: :cascade do |t|
     t.string "term"
