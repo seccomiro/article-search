7178dfcf87595617a572b02ec826c1928e1e8c70
Diego Stiehl <seccomiro@gmail.com>
2020-04-08 17:50:39 -0300

    Rename search request spec

--- spec/requests/search_request_spec.rb
+++ spec/requests/search_request_spec.rb
@@ -1,6 +1,21 @@
 require "rails_helper"
 
 RSpec.describe "Search", type: :request do
+  before(:each) do
+    DatabaseCleaner.start
+
+    create(:article, { title: "How do I cancel my subscription?" })
+    create(:article, { title: "How do I cancel my account?" })
+    create(:article, { title: "Can I upgrade my account?" })
+    create(:article, { title: "Can you help me?" })
+    create(:article, { title: "I don't know how to enroll a new person." })
+    create(:article, { title: "Is it possible to generate new users?" })
+  end
+
+  after(:each) do
+    DatabaseCleaner.clean
+  end
+
   describe "GET /search" do
     before :each do
       get search_path, params: { term: "how" }, headers: { "Accept" => "application/json" }
